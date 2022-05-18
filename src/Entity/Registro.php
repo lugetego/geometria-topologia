7e39900d6c9360c1f7f8d7ea81a7c8f6d4f02e75
lugetego <lugetego@hotmail.com>
2022-05-17 19:02:27 -0500

    recomendacion update

--- src/Entity/Registro.php
+++ src/Entity/Registro.php
@@ -482,7 +482,9 @@ class Registro
         if (null !== $recomendacion) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new \DateTimeImmutable();
+            //  $this->updatedAt = new \DateTimeImmutable();
+            $this->setModified(new \DateTime());
+
         }
     }
 
